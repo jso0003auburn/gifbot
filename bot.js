a43dac89cbcc8041211f9d8d8742e5022104c16c
jso0003auburn <john.stephen.olson@gmail.com>
2016-11-07 18:52:16 -0500

    sep

--- bot.js
+++ bot.js
@@ -9,21 +9,26 @@ var https = require('https');
 function respond() {
   this.res.writeHead(200);
   var request = JSON.parse(this.req.chunks[0]),
+  groupId = request.group_id;
   trigger = request.text.substring(0,1);
   searchTerm = request.text.substr(1).trim();
-  fullRequest = String(request.text);
+  fullRequest = request.text;
   console.log(request.name + ': ' + request.text);
   this.res.end();
+  conditions(groupId, trigger, searchTerm, fullRequest);
+}
+
+function conditions() {
 
   //Group check
-  if (request.group_id == groupIdProd) {
+  if (groupId == groupIdProd) {
     botID = botIdProd;
   } else {
   botID = botIdTest;
   }
 
   //@gifbot?
-  if (request.text.indexOf('@gifbot') >= 0) {
+  if (fullRequest.indexOf('@gifbot') >= 0) {
     requestHelp();
   } else {
   //cool
