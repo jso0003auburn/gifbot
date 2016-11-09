ea6297c5b4beab71e073b15415406bb93526b0f0
jso0003auburn <john.stephen.olson@gmail.com>
2016-11-09 13:26:53 -0500

    back

--- bot.js
+++ bot.js
@@ -81,9 +81,9 @@ function requestTicker() {
   request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + searchTerm + '%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json', function (error, response, body) {
   parsedData = JSON.parse(body);   
   last = parseFloat(parsedData.query.results.quote.LastTradePriceOnly);
-  //last = Number((last).toFixed(2));   
+  last = Number((last).toFixed(2));   
   change = parseFloat(parsedData.query.results.quote.ChangeinPercent);
-  //change = Number((change).toFixed(2));
+  change = Number((change).toFixed(2));
   name = String(parsedData.query.results.quote.Name);
   if (change > 0) {
     change = String('+' + change);
