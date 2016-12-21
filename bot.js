632d6edddf673d5947f3f4fcfc74d1acd3a7669c
jso0003auburn <john.stephen.olson@gmail.com>
2016-12-21 17:31:34 -0500

    message

--- bot.js
+++ bot.js
@@ -6,7 +6,8 @@ function respond() {
   var post = JSON.parse(this.req.chunks[0]);
   sendingGroup = post.group_id;
   sendingUser = post.name;
-  console.log(sendingUser + ' : ' + post.text);
+  message = post.text;
+  console.log(sendingUser + ' : ' + message);
   invalid = ('"' + post.text.substring(1).trim() + '" is invalid');
   this.res.writeHead(200);
 
@@ -21,13 +22,13 @@ function respond() {
   }
 
   //TAG @gifbot
-  if (post.text.indexOf('@' + process.env.botName) >= 0) {
+  if (message.indexOf('@' + process.env.botName) >= 0) {
     postMessage('GIFS = # + (search keyword)\nStocks = $ + (ticker symbol)', botId);
   }
 
   //GIF #
-  if (post.text.substring(0,1) == '#') {
-	request('https://api.giphy.com/v1/gifs/translate?s=' + post.text.substring(1).trim() + '&api_key=dc6zaTOxFJmzC&rating=r', function (error, response, body) {
+  if (message.substring(0,1) == '#') {
+	request('https://api.giphy.com/v1/gifs/translate?s=' + message.substring(1).trim() + '&api_key=dc6zaTOxFJmzC&rating=r', function (error, response, body) {
 	parsedData = JSON.parse(body);
 	if (!error && response.statusCode == 200 && parsedData && parsedData.data.images) {
 	  postMessage(parsedData.data.images.downsized.url, botId);
@@ -38,15 +39,15 @@ function respond() {
   }
 
   //STOCK TICKER $
-  if (post.text.substring(0,1) == '$') {
-    request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + post.text.substring(1).trim() + '%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json', function (error, response, body) {
+  if (message.substring(0,1) == '$') {
+    request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + message.substring(1).trim() + '%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json', function (error, response, body) {
     parsedData = JSON.parse(body); 
     if (!error && response.statusCode == 200 && String(parsedData.query.results.quote.Name) !== 'null' && String(parsedData.query.results.quote.Name) !== 'undefined') {
       change = Number((parseFloat(parsedData.query.results.quote.ChangeinPercent)).toFixed(2));
       if (change > 0) {
 	    change = String('+' + change);
       }
-	  postMessage(String(parsedData.query.results.quote.Name).substring(0,20) + '\n$' +  Number((parseFloat(parsedData.query.results.quote.LastTradePriceOnly)).toFixed(2)) + ' | ' + change + 'pct\n' + 'www.finance.yahoo.com/quote/' + post.text.substring(1).trim(), botId);
+	  postMessage(String(parsedData.query.results.quote.Name).substring(0,20) + '\n$' +  Number((parseFloat(parsedData.query.results.quote.LastTradePriceOnly)).toFixed(2)) + ' | ' + change + 'pct\n' + 'www.finance.yahoo.com/quote/' + message.substring(1).trim(), botId);
     } else {
     postMessage(invalid, botId);
     } 
