0cd8de94a34f92a30016c40bdd5a3475ccf0f1aa
jso0003auburn <john.stephen.olson@gmail.com>
2016-12-21 15:46:58 -0500

    st

--- bot.js
+++ bot.js
@@ -22,7 +22,6 @@ function respond() {
 
 //check for triggers
 function checkMessage(botId, post) {
-  searchTerm = post.text.substring(1).trim();
   console.log(post.name + ' : ' + post.text);
 
   //HELP ?
@@ -32,19 +31,19 @@ function checkMessage(botId, post) {
 
   //GIF #
   if (post.text.substring(0,1) == '#') {
-	request('https://api.giphy.com/v1/gifs/translate?s=' + searchTerm + '&api_key=dc6zaTOxFJmzC&rating=r', function (error, response, body) {
+	request('https://api.giphy.com/v1/gifs/translate?s=' + post.text.substring(1).trim() + '&api_key=dc6zaTOxFJmzC&rating=r', function (error, response, body) {
 	parsedData = JSON.parse(body);
 	if (!error && response.statusCode == 200 && parsedData && parsedData.data.images) {
 	  postMessage(parsedData.data.images.downsized.url, botId);
 	} else {
-	postMessage('"' + searchTerm + '" is invalid', botId);
+	postMessage('"' + post.text.substring(1).trim() + '" is invalid', botId);
 	}
 	});
   }
 
   //STOCK TICKER $
   if (post.text.substring(0,1) == '$') {
-    request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + searchTerm + '%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json', function (error, response, body) {
+    request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + post.text.substring(1).trim() + '%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json', function (error, response, body) {
     parsedData = JSON.parse(body); 
     if (!error && response.statusCode == 200 && String(parsedData.query.results.quote.Name) !== 'null' && String(parsedData.query.results.quote.Name) !== 'undefined') {
       change = Number((parseFloat(parsedData.query.results.quote.ChangeinPercent)).toFixed(2));
@@ -53,7 +52,7 @@ function checkMessage(botId, post) {
       }
 	  postMessage(String(parsedData.query.results.quote.Name).substring(0,20) + '\n$' +  Number((parseFloat(parsedData.query.results.quote.LastTradePriceOnly)).toFixed(2)) + ' | ' + change + 'pct\n' + 'www.finance.yahoo.com/quote/' + searchTerm, botId);
     } else {
-    postMessage('"' + searchTerm + '" is invalid', botId);
+    postMessage('"' + post.text.substring(1).trim() + '" is invalid', botId);
     } 
     }); 
   }
