a8bd5ed3fd5f41b215f3c0ad41c8c952ee4bcf0f
jso0003auburn <john.stephen.olson@gmail.com>
2016-12-20 21:19:44 -0500

    stock

--- bot.js
+++ bot.js
@@ -8,10 +8,14 @@ var groupId = process.env.groupId;
 //scan messages
 function respond() {
   var request = JSON.parse(this.req.chunks[0]);
+  
+  // define some variables
   trigger = request.text.substring(0,1);
   searchTerm = request.text.substring(1).trim();
   botTag = request.text.indexOf('@' + botName);
   console.log(request.name + ' : ' + request.text);
+
+  //check if your posting in your main group
   if (request.group_id == groupId && request.name !== botName) {
     this.res.writeHead(200);
     botId = process.env.botId;
@@ -23,13 +27,14 @@ function respond() {
   checkMessage(trigger, botTag, searchTerm, botId);
   this.res.end();
   }
+
 }
 
 //check for triggers
 function checkMessage(trigger, botTag, searchTerm, botId) {
   
   //HELP ?
-  if (trigger == '?' || botTag >= 0) {
+  if (botTag >= 0) {
     postMessage('Need help?\nStocks = $ + (ticker symbol)\nGIFS = # + (search keyword)\nTag me to see this again', botId);
   }
 
@@ -47,22 +52,22 @@ function checkMessage(trigger, botTag, searchTerm, botId) {
 
   //STOCK TICKER $
   if (trigger == '$') {
-	  request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + searchTerm + '%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json', function (error, response, body) {
-	  parsedData = JSON.parse(body); 
-	  name = String(parsedData.query.results.quote.Name.substring(0,23));  
-	  last = Number((parseFloat(parsedData.query.results.quote.LastTradePriceOnly)).toFixed(2));   
-	  change = Number((parseFloat(parsedData.query.results.quote.ChangeinPercent)).toFixed(2));
-	  if (change > 0) {
-		change = String('+' + change);
-	  }
-	  if (!error && response.statusCode == 200 && name !== 'null' && name !== 'undefined') {
-		postMessage(name + '\n$' + last + ' | ' + change + 'pct\n' + 'www.finance.yahoo.com/quote/' + searchTerm, botId);
-	  } else {
-	  postMessage('"' + searchTerm + '" is invalid\nTag me for help', botId);
-	  } 
-	  }); 
-  }
+    request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + searchTerm + '%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json', function (error, response, body) {
+    parsedData = JSON.parse(body); 
+    name = String(parsedData.query.results.quote.Name.substring(0,23));  
+    last = Number((parseFloat(parsedData.query.results.quote.LastTradePriceOnly)).toFixed(2));   
+    change = Number((parseFloat(parsedData.query.results.quote.ChangeinPercent)).toFixed(2));
+    if (change > 0) {
+	  change = String('+' + change);
+    }
+    if (!error && response.statusCode == 200 && name !== 'null' && name !== 'undefined') {
+	  postMessage(name + '\n$' + last + ' | ' + change + 'pct\n' + 'www.finance.yahoo.com/quote/' + searchTerm, botId);
+    } else {
+    postMessage('"' + searchTerm + '" is invalid\nTag me for help', botId);
+    } 
+    }); 
 }
+
 
 //Post message
 function postMessage(botResponse, botId) {
