bc7ff7abb9ab9c5ace64f0aef544ddba2541ef32
jso0003auburn <john.stephen.olson@gmail.com>
2016-12-21 14:24:30 -0500

    fix

--- bot.js
+++ bot.js
@@ -49,13 +49,15 @@ function checkMessage(trigger, botTag, searchTerm, botId) {
   if (trigger == '$') {
     request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + searchTerm + '%22)%0A%09%09&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json', function (error, response, body) {
     parsedData = JSON.parse(body); 
+    name = String(parsedData.query.results.quote.Name);
+    name = name.substring(0,23);
     last = Number((parseFloat(parsedData.query.results.quote.LastTradePriceOnly)).toFixed(2));   
     change = Number((parseFloat(parsedData.query.results.quote.ChangeinPercent)).toFixed(2));
     if (change > 0) {
 	  change = String('+' + change);
     }
-    if (!error && response.statusCode == 200 && parsedData.query.results.quote.Name. !== null) {
-	  postMessage(parsedData.query.results.quote.Name.substring(0,23) + '\n$' + last + ' | ' + change + 'pct\n' + 'www.finance.yahoo.com/quote/' + searchTerm, botId);
+    if (!error && response.statusCode == 200 && name !== 'null' && name !== 'undefined') {
+	  postMessage(name + '\n$' + last + ' | ' + change + 'pct\n' + 'www.finance.yahoo.com/quote/' + searchTerm, botId);
     } else {
     postMessage('"' + searchTerm + '" is invalid\nTag me for help', botId);
     } 
