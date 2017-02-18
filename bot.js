3b3d30585f5401da7ffb66a93492724fc91da9ca
jso0003auburn <john.stephen.olson@gmail.com>
2017-02-18 14:56:17 -0500

    e

--- bot.js
+++ bot.js
@@ -46,7 +46,7 @@ function scanMessage() {
     request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22"' + message.substring(1).trim() + '"%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=', function (error, response, body) {
     parsedData = JSON.parse(body);
     
-    if (!error && response.statusCode == 200 && parsedData.query.results !== 'undefined' && parsedData.query.results.quote.Name) {
+    if (!error && response.statusCode == 200) {
       companyName = String(parsedData.query.results.quote.Name);
       lastPrice = Number((parseFloat(parsedData.query.results.quote.LastTradePriceOnly)).toFixed(2));
       change = Number((parseFloat(parsedData.query.results.quote.ChangeinPercent)).toFixed(2));
