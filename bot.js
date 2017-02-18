60c8aacc80ab64f753b2a5b306bd1a7ace722e2c
jso0003auburn <john.stephen.olson@gmail.com>
2017-02-18 15:57:11 -0500

    res

--- bot.js
+++ bot.js
@@ -5,6 +5,9 @@ var botName = process.env.botName;
 var botId = process.env.botId;
 var botIdAlt = process.env.botIdAlt;
 var botResponseTag = 'GIFS = # + (search keyword)\nStocks = $ + (ticker symbol)';
+var gifbotTag = '@';
+var gifPostTag = '#';
+var stockPostTag = '$';
 var post = require('./post');
 
 
@@ -25,13 +28,13 @@ function respond() {
 function scanMessage() {
   
     //Was @gifbot tagged?
-  if (message.indexOf('@' + botName) >= 0) {
+  if (message.indexOf(gifbotTag + botName) >= 0) {
     botResponse = botResponseTag;
     post.postMessage(botResponse, botId);
   }
 
   //GIF #
-  if (message.substring(0,1) == '#') {
+  if (message.substring(0,1) == gifPostTag) {
 	request('https://api.giphy.com/v1/gifs/translate?s=' + message.substring(1).trim() + '&api_key=dc6zaTOxFJmzC&rating=r', function (error, response, body) {
 	parsedData = JSON.parse(body);
 	
@@ -45,7 +48,7 @@ function scanMessage() {
   }
 
   //STOCK TICKER $
-  if (message.substring(0,1) == '$') {
+  if (message.substring(0,1) == stockPostTag) {
     request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + message.substring(1).trim() + '%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=', function (error, response, body) {
     parsedData = JSON.parse(body);
     console.log(parsedData.query.results.quote.Name);
