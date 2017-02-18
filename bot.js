5ddb45f4aba2e6b2eb87bad2fbf4e6b7a66455af
jso0003auburn <john.stephen.olson@gmail.com>
2017-02-18 16:50:31 -0500

    a

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ var stockPostTag = '$';
 var post = require('./post');
 var botTag = require('./botTag');
 var gifTag = require('./gifTag');
+var stockTag = require('./stockTag');
 
 
 
@@ -37,22 +38,7 @@ function scanMessage() {
 
   //STOCK TICKER $
   if (message.substring(0,1) == stockPostTag) {
-    request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + message.substring(1).trim() + '%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=', function (error, response, body) {
-    parsedData = JSON.parse(body);
-    console.log(parsedData.query.results.quote.Name);
-    if (!error && response.statusCode == 200) {
-      companyName = String(parsedData.query.results.quote.Name);
-      lastPrice = Number((parseFloat(parsedData.query.results.quote.LastTradePriceOnly)).toFixed(2));
-      change = Number((parseFloat(parsedData.query.results.quote.ChangeinPercent)).toFixed(2));
-      if (change > 0) {
-	    change = String('+' + change);
-      }
-      botResponse = (companyName.substring(0,20) + '\n$' + lastPrice + ' | ' + change + 'pct\n' + 'www.finance.yahoo.com/quote/' + message.substring(1).trim());
-	  post.postMessage(botResponse);
-    } else {
-    console.log(message + ' is invalid');
-    } 
-    }); 
+    stockTag.stockTag();
   }
 
 }
