1e26c1bc4a468c7c35435c664c92b75b2fe54592
jso0003auburn <john.stephen.olson@gmail.com>
2017-02-18 15:38:33 -0500

    s

--- bot.js
+++ bot.js
@@ -66,30 +66,5 @@ function scanMessage() {
 
 }
 
-//posts message
-function postMessage(botResponse, botId) {
-  if (botIdAlt !== null && sendingGroup !== groupId) {
-    botId = botIdAlt;
-  } else {
-  botId = botId;
-  }
-  var options, botReq;
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST',
-    "bot_id" : botId,
-    "text" : botResponse 
-  };
-
-  botReq = https.request(options, function(res) {
-      if(res.statusCode == 202) {
-        console.log('Post success: ' + res.statusCode);
-      } else {
-      console.log('Bad status code: ' + res.statusCode);
-      }
-  });
-  botReq.end(JSON.stringify(options));
-}
 
 exports.respond = respond;
