d9bd617fb27c525e50d03173d0bbacf8569306ab
jso0003auburn <john.stephen.olson@gmail.com>
2017-03-25 01:28:05 -0400

    a

--- bot.js
+++ bot.js
@@ -5,8 +5,6 @@ var groupIdMain = process.env.groupIdMain;
 var botIdMain = process.env.botIdMain;
 var groupIdAlt = process.env.groupIdAlt;
 var botIdAlt = process.env.botIdAlt;
-var gifTag = require('./gifTag');
-
 
 
 //processes incoming groupme posts
@@ -57,6 +55,20 @@ function botTag(botId) {
   postMessage(botResponse, botId);
 }
 
+//posts message
+function gifTag(botId) {
+  request('https://api.giphy.com/v1/gifs/translate?s=' + message.substring(1).trim() + '&api_key=dc6zaTOxFJmzC&rating=r', function (error, response, body) {
+  parsedData = JSON.parse(body);
+  
+  if (!error && response.statusCode == 200 && parsedData && parsedData.data.images) {
+	botResponse = parsedData.data.images.downsized.url;
+	//console.log('GIF size: ' + parsedData.data.images.downsized.size);
+	postMessage(botResponse, botId);
+  } else {
+  console.log(message + ' is invalid');
+  }
+  });
+}
 
 //posts message
 function stockTag(botId) {
@@ -80,7 +92,8 @@ function stockTag(botId) {
 
 //posts message
 function postMessage(botResponse, botId) {
-  	
+  
+
   var options, botReq;
   options = {
     hostname: 'api.groupme.com',
