08f1c9fb0dfa9c69afcbf90a94504dec7864444e
jso0003auburn <john.stephen.olson@gmail.com>
2017-03-24 23:37:26 -0400

    a

--- bot.js
+++ bot.js
@@ -74,7 +74,7 @@ function gifTag(botId) {
 function stockTag(botId) {
   request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + message.substring(1).trim() + '%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=', function (error, response, body) {
   parsedData = JSON.parse(body);
-  if (parsedData.query.results.quote.Name = null) {
+  if (parsedData.query.results.quote.Name == 'null') {
     botResponse = ('invalid');
     postMessage(botResponse, botID);
   }
