8adf312eef8e2fbae3189371f08860c2a9377109
jso0003auburn <john.stephen.olson@gmail.com>
2017-04-13 19:16:03 -0400

    a

--- bot.js
+++ bot.js
@@ -85,7 +85,7 @@ function stockTag(botId) {
 	  change = String('+' + change);
 	}
 	console.log(parsedData);
-	botResponse = (symbol + ': $' + lastPrice + ' | ' + change + 'pct\n' + companyName.substring(0,20) + '\n' + 'www.finance.yahoo.com/quote/' + message.substring(1).trim());
+	botResponse = ('$' + lastPrice + ' | ' + change + 'pct\n' + symbol + ' |  ' + companyName.substring(0,15) + '\n' + 'www.finance.yahoo.com/quote/' + message.substring(1).trim());
 	postMessage(botResponse, botId);
   } else {
   console.log(message + ' is invalid');
