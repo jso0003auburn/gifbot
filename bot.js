ed802613df384050d9364588fab31578a717e455
jso0003auburn <john.stephen.olson@gmail.com>
2017-04-13 21:43:08 -0400

    a

--- bot.js
+++ bot.js
@@ -41,10 +41,7 @@ function respond() {
     gifTag(botId);
   }
 
-  //STOCK TICKER $
-  if (message.substring(0,1) == '$' && botId !== '1') {
-    stockTag(botId);
-  }  
+
   console.log(sendingUser + ' : ' + message);
   this.res.end();
 }
@@ -70,25 +67,6 @@ function gifTag(botId) {
   });
 }
 
-//posts message
-function stockTag(botId) {
-  //https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + message.substring(1).trim() + '%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
-  request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + message.substring(1).trim() + '%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=', function (error, response, body) {
-  parsedData = JSON.parse(body);
-  if (!error && response.statusCode == 200 && parsedData.query.results.quote.Name !== null && parsedData.query.results.quote.PercentChange !== null) {
-	companyName = String(parsedData.query.results.quote.Name);
-	lastPrice = Number((parseFloat(parsedData.query.results.quote.LastTradePriceOnly)).toFixed(2));
-	change = Number((parseFloat(parsedData.query.results.quote.PercentChange)).toFixed(2));
-	if (change > 0) {
-	  change = String('+' + change);
-	}
-	botResponse = ('$' + lastPrice + ' | ' + change + 'pct\n'+ companyName.substring(0,15) + '\n' + 'www.finance.yahoo.com/quote/' + message.substring(1).trim());
-	postMessage(botResponse, botId);
-  } else {
-  console.log(message + ' is invalid');
-  } 
-  }); 
-}
 
 //posts message
 function postMessage(botResponse, botId) {
