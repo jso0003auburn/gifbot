b9609d566fe6e9da53f239c4babc4683c4bd7458
jso0003auburn <john.stephen.olson@gmail.com>
2017-04-13 21:26:53 -0400

    a

--- bot.js
+++ bot.js
@@ -81,7 +81,7 @@ function stockTag(botId) {
 	if (change > 0) {
 	  change = String('+' + change);
 	}
-	botResponse = ('$' + lastPrice + ' | ' + change + 'pct\n'+ companyName.substring(0,15) + '\n' + 'www.finance.yahoo.com/quote/' + message.substring(1).trim());
+	botResponse = ('$' + lastPrice + ' | ' + change + 'pct' + '\n' + companyName.substring(0,15) + '\n' + 'www.finance.yahoo.com/quote/' + message.substring(1).trim());
 	postMessage(botResponse, botId);
   } else {
   console.log(message + ' is invalid');
@@ -92,7 +92,6 @@ function stockTag(botId) {
 //posts message
 function postMessage(botResponse, botId) {
   
-
   var options, botReq;
   options = {
     hostname: 'api.groupme.com',
