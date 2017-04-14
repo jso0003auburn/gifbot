1727fc98fbb0094f52b06bde0932dfface3b1efe
jso0003auburn <john.stephen.olson@gmail.com>
2017-04-13 21:37:30 -0400

    a

--- bot.js
+++ bot.js
@@ -72,17 +72,18 @@ function gifTag(botId) {
 
 //posts message
 function stockTag(botId) {
+  //https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + message.substring(1).trim() + '%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=
   request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + message.substring(1).trim() + '%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=', function (error, response, body) {
   parsedData = JSON.parse(body);
-  
   if (!error && response.statusCode == 200 && parsedData.query.results.quote.Name !== null) {
 	companyName = String(parsedData.query.results.quote.Name);
 	lastPrice = Number((parseFloat(parsedData.query.results.quote.LastTradePriceOnly)).toFixed(2));
+	symbol = String(parsedData.query.results.quote.Symbol);
 	change = Number((parseFloat(parsedData.query.results.quote.PercentChange)).toFixed(2));
 	if (change > 0) {
 	  change = String('+' + change);
 	}
-	botResponse = ('$' + lastPrice + '\n' + companyName.substring(0,15) + '\n' + 'www.finance.yahoo.com/quote/' + message.substring(1).trim());
+	botResponse = ('$' + lastPrice + ' | ' + change + 'pct\n'+ companyName.substring(0,15) + '\n' + 'www.finance.yahoo.com/quote/' + message.substring(1).trim());
 	postMessage(botResponse, botId);
   } else {
   console.log(message + ' is invalid');
@@ -93,6 +94,7 @@ function stockTag(botId) {
 //posts message
 function postMessage(botResponse, botId) {
   
+
   var options, botReq;
   options = {
     hostname: 'api.groupme.com',
