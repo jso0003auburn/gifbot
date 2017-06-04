7e5b0c3af9c4011472a1dc36586ec7cbbb8d5f2c
jso0003auburn <john.stephen.olson@gmail.com>
2017-06-04 01:59:30 -0400

    a

--- bot.js
+++ bot.js
@@ -75,19 +75,18 @@ function gifTag(botId) {
 function stockTag(botId) {
   request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + message.substring(1).trim() + '%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=', function (error, response, body) {
   parsedData = JSON.parse(body);
-  if (parsedData.query.results.quote.Name !== undefined || parsedData.query.results.quote.Name !== null) {
-	companyName = String(parsedData.query.results.quote.Name);
-	deets = companyName;
-	lastPrice = Number((parseFloat(parsedData.query.results.quote.LastTradePriceOnly)).toFixed(2));
-	change = Number((parseFloat(parsedData.query.results.quote.PercentChange)).toFixed(2));
-	if (change > 0) {
-	  change = String('+' + change);
-	}
-	botResponse = (companyName.substring(0,20) + '\n$' + lastPrice + ' / ' + change + '\n' + 'www.finance.yahoo.com/quote/' + message.substring(1).trim());
-	postMessage(botResponse, botId);
+  if (parsedData.query.results.quote.Name == undefined || parsedData.query.results.quote.Name == null) {
+	console.log(message + ' is invalid');
   } else {
-  console.log(message + ' is invalid');
-  } 
+  companyName = String(parsedData.query.results.quote.Name);
+  deets = companyName;
+  lastPrice = Number((parseFloat(parsedData.query.results.quote.LastTradePriceOnly)).toFixed(2));
+  change = Number((parseFloat(parsedData.query.results.quote.PercentChange)).toFixed(2));
+  if (change > 0) {
+    change = String('+' + change);
+  }
+  botResponse = (companyName.substring(0,20) + '\n$' + lastPrice + ' / ' + change + '\n' + 'www.finance.yahoo.com/quote/' + message.substring(1).trim());
+  postMessage(botResponse, botId);  } 
   }); 
 }
 
