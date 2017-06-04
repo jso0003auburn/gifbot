7944bada2790a306366817a1820a102d1743fed6
jso0003auburn <john.stephen.olson@gmail.com>
2017-06-04 01:53:33 -0400

    revert

--- bot.js
+++ bot.js
@@ -75,7 +75,7 @@ function gifTag(botId) {
 function stockTag(botId) {
   request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + message.substring(1).trim() + '%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=', function (error, response, body) {
   parsedData = JSON.parse(body);
-  if (!error && response.statusCode == 200 && (parsedData.query.results.quote.Name !== undefined || parsedData.query.results.quote.Name !== null) {
+  if (!error && response.statusCode == 200 && parsedData.query.results.quote.Name !== undefined) {
 	companyName = String(parsedData.query.results.quote.Name);
 	deets = companyName;
 	lastPrice = Number((parseFloat(parsedData.query.results.quote.LastTradePriceOnly)).toFixed(2));
