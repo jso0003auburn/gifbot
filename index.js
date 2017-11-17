856dc2f9a146060c89a21371f365a3fc4d6e5d4b
John <john.stephen.olson@gmail.com>
2017-11-16 20:42:08 -0500

    a

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ server = http.createServer(function (req, res) {
   });
 });
 
-port = Number(process.env.PORT || 5000);
+port = Number(process.env.PORT || 3000);
 server.listen(port);
 
 function ping() {
