0e326a388502f12570b73546d7947cb685e7bd5d
John <john.stephen.olson@gmail.com>
2017-12-03 14:58:27 -0500

    simpler

--- bot.js
+++ bot.js
@@ -40,18 +40,11 @@ function respond() {
   if (message.substring(0,1) == '#' && botId !== '1') {
     gifTag(botId);
   }
-
-  //STOCK TICKER $
-  if (message.substring(0,1) == '$' && botId !== '1') {
-    stockTag(botId);
-  }  
-  console.log(sendingUser + ' : ' + message);
-  this.res.end();
 }
 
 //if @gifbot was tagged this will post a help message
 function botTag(botId) {
-  botResponse = 'to post a GIF type #lol \nfor stock quotes type #BAC';
+  botResponse = 'to post a GIF: #lol';
   deets = 'gifbot';
   postMessage(botResponse, botId);
 }
@@ -69,26 +62,6 @@ function gifTag(botId) {
   console.log(message + ' is invalid');
   }
   });
-}
-
-//posts message
-function stockTag(botId) {
-  request('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20(%22' + message.substring(1).trim() + '%22)&format=json&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=', function (error, response, body) {
-  parsedData = JSON.parse(body);
-  if (parsedData.query.results.quote.Name == undefined || parsedData.query.results.quote.Name == null) {
-    console.log(message + ' is invalid');
-  } else {
-  companyName = String(parsedData.query.results.quote.Name);
-  companyName = companyName.substring(0,15);
-  deets = companyName;
-  lastPrice = Number((parseFloat(parsedData.query.results.quote.LastTradePriceOnly)).toFixed(2));
-  change = Number((parseFloat(parsedData.query.results.quote.PercentChange)).toFixed(2));
-  if (change > 0) {
-    change = String('+' + change);
-  }
-  botResponse = (companyName + '\n$' + lastPrice + ' / ' + change + '\n' + 'www.finance.yahoo.com/quote/' + message.substring(1).trim());
-  postMessage(botResponse, botId);  } 
-  }); 
 }
 
 //posts message
