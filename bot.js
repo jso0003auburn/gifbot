ac50cf8a27979bebba3abcaf2b0e39b39b1ae907
jso0003auburn <john.stephen.olson@gmail.com>
2018-09-28 19:48:27 -0400

    a

--- bot.js
+++ bot.js
@@ -73,10 +73,11 @@ function gifTag(botId) {
 
 function stockTag(botId) {
   request('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + message.substring(1).trim() + '&outputsize=compact&apikey=528P3B6Q2EW4I7B3', function (error, response, body) {
-  parsedData = JSON.parse(body, function (key, lastRefreshed) {
-    if (key == "2019-09-28") {
-      lastRefreshed = JSON.stringify(lastRefreshed);
-      console.log(lastRefreshed);
+  parsedData = JSON.parse(body, function (key, value) {
+    if (key == "4. close") {
+      console.log(value);
+      value = value[1];
+      console.log(value
     } else {
       //console.log('error parsing');
     }});
