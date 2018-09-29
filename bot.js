45a504e743eecc69f1d236cecd8d26abdb1fd19a
jso0003auburn <john.stephen.olson@gmail.com>
2018-09-28 20:41:47 -0400

    a

--- bot.js
+++ bot.js
@@ -75,8 +75,8 @@ function stockTag(botId) {
   request('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + message.substring(1).trim() + '&outputsize=compact&apikey=528P3B6Q2EW4I7B3', function (error, response, body) {
   x = body;
   console.log(x);
-  //y = body["Time Series (Daily)"];
-  //console.log(y);
+  y = x["Time Series (Daily)"]["2018-09-28"]["4. close"];
+  console.log(y);
   }); 
 }
 //posts message
