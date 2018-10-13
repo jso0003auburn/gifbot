777a4c9ec8585b46dcb861c32c23cb5e4a7aa27a
jso0003auburn <john.stephen.olson@gmail.com>
2018-10-13 18:35:31 -0400

    a

--- bot.js
+++ bot.js
@@ -70,7 +70,7 @@ function gifTag(botId) {
 function stockTag(botId) {
   request('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + message.substring(1).trim() + '&outputsize=compact&apikey=528P3B6Q2EW4I7B3', function (error, response, body) {
   parsedData = JSON.parse(body);
-  console.log(Object.values(parsedData).indexOf('Invalid'));
+  console.log(Object.values(parsedData).indexOf('Error Message'));
 
   if (!error && response.statusCode == 200 && Object.values(parsedData).indexOf('Invalid') >= 1 && parsedData !== 'undefined') {
     lastRefreshed = parsedData['Meta Data']['3. Last Refreshed'];
