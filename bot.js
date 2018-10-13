188959347e645b82566b8404a5518a66db5257b5
jso0003auburn <john.stephen.olson@gmail.com>
2018-10-13 18:13:56 -0400

    a

--- bot.js
+++ bot.js
@@ -71,7 +71,7 @@ function stockTag(botId) {
   request('https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + message.substring(1).trim() + '&outputsize=compact&apikey=528P3B6Q2EW4I7B3', function (error, response, body) {
   parsedData = JSON.parse(body);
 
-  if (!error && response.statusCode == 200 && parsedData && parsedData !== 'undefined') {
+  if (!error && response.statusCode == 200 && response.statusCode !== 401 && parsedData && parsedData !== 'undefined') {
     lastRefreshed = parsedData['Meta Data']['3. Last Refreshed'];
     lastRefreshed = lastRefreshed.substring(0,10);
     close = Number(parsedData['Time Series (Daily)'][lastRefreshed]['4. close']);
