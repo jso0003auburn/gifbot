0a4267a892d345783dc40cbed843b85cbd806f22
jso0003auburn <john.stephen.olson@gmail.com>
2018-11-02 18:33:38 -0400

    a

--- bot.js
+++ bot.js
@@ -82,12 +82,14 @@ function stockTag(botId) {
     lastRefreshed = quoteObj['Global Quote']['07. latest trading day'];
     change = quoteObj['Global Quote']['10. change percent'].slice(0,-3);
     change = Number(change);
+    pct = String('%');
+    pct = utf8.encode(pct);
     if (quoteObj['Global Quote']['10. change percent'].substring(0,1) == '-') {
      //change = change;
     } else {
     change = '+' + change;
     }
-    botResponse = ('$' + price + '\n' + change + 'pct\n' + 'https://finance.yahoo.com/quote/' + message.substring(1));
+    botResponse = ('$' + price + '\n' + change + pct + '\n' + 'https://finance.yahoo.com/quote/' + message.substring(1));
     postMessage(botResponse, botId);
   } else {
   console.log(message + ' is invalid');
