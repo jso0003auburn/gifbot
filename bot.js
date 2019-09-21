7d5a863c82065f22844ca4404bf67d82727c0712
ErikBoesen <me@erikboesen.com>
2019-09-21 12:10:20 -0400

    Clean up everything and use MeBots

--- bot.js
+++ bot.js
@@ -1,211 +1,143 @@
 var request = require('request');
 var https = require('https');
+var mebots = require('mebots');
 
 // https://dev.groupme.com/bots
 // https://dashboard.heroku.com/apps/groupme-gif-bot/settings
 var botName = process.env.botName;
 var alphaVantageAPIKey = process.env.alphaVantageAPIKey;
-var groupMeAppToken = process.env.groupMeAppToken;
 var giphyAPIKey = process.env.giphyAPIKey;
-var botId = '1';
 
-// - processes incoming groupme posts
+// Process incoming groupme messages
 function respond() {
-  var post = JSON.parse(this.req.chunks[0]);
-  this.res.writeHead(200);
+    var message = JSON.parse(this.req.chunks[0]);
+    this.res.writeHead(200);
 
-  sendingGroup = post.group_id;
-  sendingUser = post.name;
-  message = post.text;
-  messageTrimmed = message.substring(1).trim();
-
-  //From the main group?
-  mainGroupId = process.env.mainGroupId;
-  if (sendingGroup == mainGroupId) {
-    botId = process.env.mainBotId;
-    groupName = process.env.mainGroupName;
-    rating = process.env.mainRating;
-  }
-
-
-
-  //from the Test group? (Olson Test)
-  testGroupId = process.env.testGroupId;
-  if (sendingGroup == testGroupId) {
-    botId = process.env.testBotId;
-    groupName = process.env.testGroupName;
-    rating = process.env.testRating;
-  }
-
-
-
-  //from the 2 group WOLFPACK?
-  group2Id = process.env.group2Id;
-  if (sendingGroup == group2Id) {
-    botId = process.env.group2BotId;
-    groupName = process.env.group2Name;
-    rating = process.env.group2Rating;
-  }
-
-
-
-  //from the 3 group OLSON FAMILY?
-  group3Id = process.env.group3Id;
-  if (sendingGroup == group3Id) {
-    botId = process.env.group3BotId;
-    groupName = process.env.group3Name;
-    rating = process.env.group3Rating;
-  }
-
-
-
-  // LOGGING
-  //from an unrecognized group?
-  if (botId == '1') {
-    console.log(message + ' sent without a valid group id: ' + sendingGroup);
-    return;
-  }
-
-  //sent from the bot?
-  if (sendingUser == botName) {
-    console.log(groupName.padEnd(15," -") + sendingUser.substring(0,10).padEnd(15," -") + 'SENT: something');
-    return;
-  }
-
-  //sent from not the bot
-  if (sendingUser !== botName) {
-    //console.log(sendingUser.substring(0,10).padEnd(11) + 'SENT: ' + message.substring(0,50).padEnd(53," . ") + ' IN: ' + groupName);
-    console.log(groupName.padEnd(15," -") + sendingUser.substring(0,10).padEnd(15," -") + 'SENT: ' + message);
-    tagCheck(botId);
-  }
-
-
+    console.log(message.group_id + ' @' + message.name + ': ' + message.text);
+    if (message.sender_type != 'bot') {
+        tagCheck(message);
+    }
 }
 
 
-
-
-function tagCheck(botId) {
-
-
-  //Was the bot tagged?
-  if (message.indexOf('@' + botName) >= 0 && botId !== '1') {
-    botTag(botId);
-  }
-
-  //GIF #
-  if (message.substring(0,1) == '#' && botId !== '1') {
-    gifTag(botId);
-  }
-
-  //Stock $
-  if (message.substring(0,1) == '$' && botId !== '1') {
-    stockTag(botId);
-  }
-  
-  //GIF #
-  if (message.substring(0,1) == '^' && botId !== '1') {
-    mlbTag(botId);
-  }
+function trim(text) {
+    return text.substring(1).trim();
 }
 
 
-
-
-//was the bot tagged
-function botTag(botId) {
-
-    request('https://braves-groupme.appspot.com/CHECK?groupName=' + groupName + '&teamKey=ATL', function (error, response, body) {
-    console.log(response.statusCode);
-    if (response.statusCode == 201) {
-        botTagResponse = 'try #auburn basketball for a gif\ntry $bac for a stock price';
-        botTagResponseLog = 'I was tagged by: ' + sendingUser;
-        botResponse = botTagResponse;
-        specificLog = botTagResponseLog;
-        postMessage(botResponse, botId);
-    }      
-    });
-}
-
-//posts message
-function gifTag(botId) {
-  request('https://api.giphy.com/v1/gifs/translate?s=' + messageTrimmed + '&api_key=' + giphyAPIKey + '&rating=' + rating, function (error, response, body) {
-  parsedData = JSON.parse(body);
-  //did they use spaces?
-  spaceCount = (message.split(" ").length - 1);
-  if (spaceCount < 1 && messageTrimmed.length > 12) {
-    console.log('too long - space count ' + spaceCount + ' message length: ' + messageTrimmed.length + ' status: ' + response.statusCode);
-  }
-
-
-
-  if (!error && response.statusCode == 200 && parsedData && parsedData.data.images) {
-    botResponse = parsedData.data.images.fixed_width.url;
-    //downsized = parseFloat(parsedData.data.images.downsized.size).toLocaleString('en');
-    specificLog = ('Fixed Size: ' + parseFloat(parsedData.data.images.fixed_width.size).toLocaleString('en') + ' Rating: ' + parsedData.data.rating + ' Giphy Status: ' + response.statusCode);
-    postMessage(botResponse, botId);
-  }
-  });
-}
-
-
-//stock quote
-function stockTag(botId) {
-  request('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + messageTrimmed + '&outputsize=compact&apikey=' + alphaVantageAPIKey, function (error, response, body) {
-  quoteObj = JSON.parse(body);
-  if (!error && quoteObj && Number(quoteObj['Global Quote']['05. price']) == Number(quoteObj['Global Quote']['05. price'])) {
-    open = Number(quoteObj['Global Quote']['02. open']);
-    price = Number(quoteObj['Global Quote']['05. price']);
-    lastRefreshed = quoteObj['Global Quote']['07. latest trading day'];
-    change = quoteObj['Global Quote']['10. change percent'].slice(0,-3);
-    change = Number(change);
-    if (quoteObj['Global Quote']['10. change percent'].substring(0,1) == '-') {
-     //change = change;
-    } else {
-    change = '+' + change;
+function tagCheck(message) {
+    // Was the bot tagged?
+    if (message.toLowerCase().indexOf('@gifbot') >= 0) {
+        botTag(groupID);
     }
 
-    botResponse = ('$' + price + '\n' + change + 'pct\n' + 'https://finance.yahoo.com/quote/' + messageTrimmed);
-    specificLog = (messageTrimmed + ' ' + price + ' ' + change + ' alphavantage status: ' + response.statusCode);
-    postMessage(botResponse, botId);
-  } else {
-  console.log(groupName + ' - ' + message + ' is invalid');
-  }
-  });
+    // GIF #
+    if (message.substring(0,1) == '#') {
+        gifTag(groupID);
+    }
+
+    // Stock $
+    if (message.substring(0,1) == '$') {
+        stockTag(groupID);
+    }
+
+    // GIF #
+    if (message.substring(0,1) == '^') {
+        mlbTag(groupID);
+    }
 }
 
-//was the bot tagged with an MLB team?
-function mlbTag(botId) {
-    messageTrimmed = messageTrimmed.toUpperCase();
-    request('https://braves-groupme.appspot.com/CHECK?groupName=' + groupName + '&teamKey=' + messageTrimmed, function (error, response, body) {
-    console.log(response.statusCode);
-    if (response.statusCode == 500) {
-        botTagResponse = 'https://braves-groupme.appspot.com/';
-    }      
+
+// If the bot was tagged
+function botTag(message) {
+    request('https://braves-groupme.appspot.com/CHECK?groupName=' + message.group_id + '&teamKey=ATL', function (error, response, body) {
+        if (response.statusCode == 201) {
+            botTagResponse = 'try #auburn basketball for a gif\ntry $bac for a stock price';
+            botTagResponseLog = 'I was tagged by: ' + sendingUser;
+            botResponse = botTagResponse;
+            specificLog = botTagResponseLog;
+            postMessage(botResponse, message.group_id);
+        }
     });
 }
-//posts message
-function postMessage(botResponse, botId) {
-  
-  var options, botReq;
-  options = {
-    hostname: 'api.groupme.com',
-    path: '/v3/bots/post',
-    method: 'POST',
-    "token" : groupMeAppToken,
-    "bot_id" : botId,
-    "text" : botResponse 
-  };
 
-  botReq = https.request(options, function(res) {
-      if(res.statusCode == 202) {
-        console.log(groupName.padEnd(15," -") + botName.substring(0,10).padEnd(15," -") + 'POSTED: ' + specificLog.substring(0,48) + ' GroupMe Status: ' + res.statusCode);
-        //console.log(botName.substring(0,10).padEnd(11) + 'POSTED: ' + specificLog.substring(0,48).padEnd(51," . ") + ' IN: ' + groupName + ' - STATUS: ' + res.statusCode);
-      } else {
-      console.log('Error posting to: ' + groupName + ' - LOG - Bad status code: ' + res.statusCode + ' messageTrimmed: ' + messageTrimmed);
-      }
-  });
-  botReq.end(JSON.stringify(options));
+// If a GIF was requested
+function gifTag(message) {
+    request('https://api.giphy.com/v1/gifs/translate?s=' + trim(message.text) + '&api_key=' + giphyAPIKey, function (error, response, body) {
+        parsedData = JSON.parse(body);
+        // Did they use spaces?
+        spaceCount = (message.split(' ').length - 1);
+        if (spaceCount < 1 && messageTrimmed.length > 12) {
+            console.log('too long - space count ' + spaceCount + ' message length: ' + trim(message.text)length + ' status: ' + response.statusCode);
+        }
+
+        if (!error && response.statusCode == 200 && parsedData && parsedData.data.images) {
+            botResponse = parsedData.data.images.fixed_width.url;
+            //downsized = parseFloat(parsedData.data.images.downsized.size).toLocaleString('en');
+            specificLog = ('Fixed Size: ' + parseFloat(parsedData.data.images.fixed_width.size).toLocaleString('en') + ' Rating: ' + parsedData.data.rating + ' Giphy Status: ' + response.statusCode);
+            postMessage(botResponse, message.group_id);
+        }
+    });
+}
+
+
+// Stock quote
+function stockTag(groupID) {
+    request('https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=' + messageTrimmed + '&outputsize=compact&apikey=' + alphaVantageAPIKey, function (error, response, body) {
+        quoteObj = JSON.parse(body);
+        if (!error && quoteObj && Number(quoteObj['Global Quote']['05. price']) == Number(quoteObj['Global Quote']['05. price'])) {
+            open = Number(quoteObj['Global Quote']['02. open']);
+            price = Number(quoteObj['Global Quote']['05. price']);
+            lastRefreshed = quoteObj['Global Quote']['07. latest trading day'];
+            change = quoteObj['Global Quote']['10. change percent'].slice(0,-3);
+            change = Number(change);
+            if (quoteObj['Global Quote']['10. change percent'].substring(0,1) == '-') {
+                //change = change;
+            } else {
+                change = '+' + change;
+            }
+
+            botResponse = ('$' + price + '\n' + change + 'pct\n' + 'https://finance.yahoo.com/quote/' + trim(message.text));
+            specificLog = (trim(message.text) + ' ' + price + ' ' + change + ' alphavantage status: ' + response.statusCode);
+            postMessage(botResponse, botId);
+        } else {
+            console.log(message.group_id + ' - ' + message.text + ' is invalid');
+        }
+    });
+}
+
+// Was the bot tagged with an MLB team?
+function mlbTag(groupID) {
+    messageTrimmed = trim(message.text).toUpperCase();
+    request('https://braves-groupme.appspot.com/CHECK?groupName=' + message.group_id + '&teamKey=' + messageTrimmed, function (error, response, body) {
+        console.log(response.statusCode);
+        if (response.statusCode == 500) {
+            botTagResponse = 'https://braves-groupme.appspot.com/';
+        }
+    });
+}
+
+// Post message
+function postMessage(text, groupID) {
+    bot.instance(groupID).then((instance) => {
+        var options, botReq;
+        options = {
+            hostname: 'api.groupme.com',
+            path: '/v3/bots/post',
+            method: 'POST',
+            'bot_id': instance.id,
+            'text': text
+        };
+
+        botReq = https.request(options, function(res) {
+            if(res.statusCode == 202) {
+                console.log(groupID.padEnd(15,', POSTED -') + 'POSTED: ' + text);
+            } else {
+                console.log('Error posting to: ' + groupID + ' - LOG - Bad status code: ' + res.statusCode + ' message: ' + text);
+            }
+        });
+        botReq.end(JSON.stringify(options));
+    });
 }
 
 exports.respond = respond;
--- index.js
+++ index.js
@@ -5,28 +5,28 @@ director    = require('director');
 bot         = require('./bot.js');
 
 router = new director.http.Router({
-  '/' : {
-    post: bot.respond,
-    get: ping
-  }
+    '/' : {
+        post: bot.respond,
+        get: ping
+    }
 });
 
 server = http.createServer(function (req, res) {
-  req.chunks = [];
-  req.on('data', function (chunk) {
-    req.chunks.push(chunk.toString());
-  });
+    req.chunks = [];
+    req.on('data', function (chunk) {
+        req.chunks.push(chunk.toString());
+    });
 
-  router.dispatch(req, res, function(err) {
-    res.writeHead(err.status, {"Content-Type": "text/plain"});
-    res.end(err.message);
-  });
+    router.dispatch(req, res, function(err) {
+        res.writeHead(err.status, {"Content-Type": "text/plain"});
+        res.end(err.message);
+    });
 });
 
 port = Number(process.env.PORT || 3000);
 server.listen(port);
 
 function ping() {
-  this.res.writeHead(200);
-  this.res.end("I am gifbot");
+    this.res.writeHead(200);
+    this.res.end("I am gifbot");
 }
