14e12931d711bb452581dca4833624613156d017
jso0003auburn <john.stephen.olson@gmail.com>
2019-09-26 23:29:32 -0400

    a

--- bot.js
+++ bot.js
@@ -64,9 +64,13 @@ function botTag(message) {
 
 // If a GIF was requested
 function gifTag(message) {
+    
     request('https://api.giphy.com/v1/gifs/search?s=' + trim(message.text) + '&api_key=' + giphyAPIKey, function (error, response, body) {
         parsedData = JSON.parse(body);
         console.log('Giphy Status: ' + response.statusCode);
+        console.log('Search: ' + parsedData.data.images.fixed_width.url);
+        console.log('Search: GIF Fixed Size: ' + parseFloat(parsedData.data.images.fixed_width.size).toLocaleString('en'));
+        console.log('Search: Rating: ' + parsedData.data.rating + ' Giphy Status: ' + response.statusCode);
         if (!error && response.statusCode == 200 && parsedData && parsedData.data.images) {
             console.log('Search: ' + parsedData.data.images.fixed_width.url);
             console.log('Search: GIF Fixed Size: ' + parseFloat(parsedData.data.images.fixed_width.size).toLocaleString('en'));
